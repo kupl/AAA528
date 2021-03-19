1bc481dbe3c07ff9cfe1ab9ae11efb166d037e1a
Heewoong Jang <jormal@naver.com>
2021-03-19 19:06:32 +0900

    feat(template): Add template for student
    Template for student code is added.

--- hw3/src/main.ml
+++ hw3/src/main.ml
@@ -1,13 +1,19 @@
-(* Main Entrancy Function of HW3 *)
+(* Main Entrance Function of HW3 *)
 
 let main : unit -> unit
 = fun () -> begin
   (* main function start *)
-  ()
+  (* 1. Read Input File *)
+  let c = Libs.Io.Input.read () in
+  (* 2. Run Verifier*)
+  let v = Hw3.verify c in
+  (* 3. Print Result *)
+  Libs.Io.Output.read v
   (* main function end *)
 end
 
 let _ = begin
+  Utils.Args.create ();
   Printexc.record_backtrace true;
   try
     main ()
